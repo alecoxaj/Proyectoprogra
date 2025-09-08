f1b793895b0701fbbf4e9a4ecdd3735c5a13c1c8
José Noguera <juniornoguera1@gmail.com>
2025-09-07 18:35:29 -0600

    Clase para guardar usuarios

---
+++ Clases.py
@@ -0,0 +1,25 @@
+class Usuario:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.usuarios = {}
+
+    def CargarUsuarios(self):
+        try:
+            with open("usuarios.txt", "r", encoding="utf-8") as archivo:
+                for liena in archivo:
+                    linea = linea.strip()
+                    if linea:
+                        cui, nombre, rol = linea.split(":")
+                        self.usuarios[cui] = {
+                            'cui': cui,
+                            'nombre': nombre,
+                            'rol': rol
+                        }
+            print("Se importaron usuarios de usuarios.txt")
+        except FileNotFoundError:
+            print("No existe este archivo") # cambiar por tkinter
+
+    def GuardarUsuarios(self):
+        with open("usuarios.txt", "w", encoding="utf=8") as archivo:
+            for cui, datos in self.usuarios.items():
+                archivo.write(f"{cui}:{datos['nombre']}:{datos['rol']}")
